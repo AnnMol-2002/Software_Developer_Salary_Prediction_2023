5e7f37c2b6347ef3a701fd1556b572c3f96a4ab3
Ann Mol <83901287+AnnMol-2002@users.noreply.github.com>
2023-09-09 15:39:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from predict import show_predict
+from explore import show_explore
+
+page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+
+if page == "Predict":
+    show_predict()
+else:
+    show_explore()
